6271f0cb7787fc0ffd920c302359f6bfef9a6d88
dearguru07 <pangalaguruyadav101@gmail.com>
2024-11-27 16:47:02 +0530

    js

---
+++ app.js
@@ -0,0 +1,37 @@
+// console.log('Hello world.....')
+
+// var a=5
+// function GetName(){
+//     console.log('Namastee JS.....')
+// }
+
+// GetName()
+// console.log(a)
+// console.log(GetName)
+// console.log(Fun1)
+// var Fun1=function Name(){
+//     console.log(4*5)
+// }
+
+
+// var a = 1;
+// b();
+// c();
+// console.log(a);
+// function b() {
+//   var a = 10;
+//   console.log(a);
+// }
+// function c() {
+//   var a = 100;
+//   console.log(a);
+// }
+
+// const value='458'
+// const res=typeof(value)+value
+// console.log(res)
+
+let heading=document.getElementById('heading')
+heading.onclick=()=>{
+  heading.innerText='Namstee Guru'
+}
