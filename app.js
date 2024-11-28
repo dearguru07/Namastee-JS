de71b5a5c72420e4679404258169b3d2efcc0976
dearguru07 <pangalaguruyadav101@gmail.com>
2024-11-28 16:25:27 +0530

    k

--- app.js
+++ app.js
@@ -31,7 +31,45 @@
 // const res=typeof(value)+value
 // console.log(res)
 
-let heading=document.getElementById('heading')
-heading.onclick=()=>{
-  heading.innerText='Namstee Guru'
+
+// const heading=document.getElementById('heading')
+// heading.onclick=()=>{
+//   heading.innerText='Namstee Guru'
+// }
+
+// function a(){
+//   console.log(c)
+// }
+// const c=10
+// a()
+
+
+// function a(){
+//   console.log(c)
+//   const c=20
+// }
+// a()
+
+
+
+// console.error('Hello');
+
+// x()
+// function x(){
+//     console.log(10)
+// }
+
+// function a(){
+//     let s=10
+//     function b(){
+//         console.log(s)
+//     }
+//     return b
+// }
+// let res=a()
+// console.log(res)
+
+console.log(a)
+function a(){
+    console.log(154)
 }
