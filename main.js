37783d2225ebf7e765953845f62fc5637e78e3fe
dearguru07 <pangalaguruyadav101@gmail.com>
2025-02-02 20:05:04 +0530

    doi

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-z
+
+
+console.log('Start')
+let res=document.getElementById('btn').addEventListener('click',()=>{
+  console.log('Namstee JS......')
+})
+
+console.log('End ......')
 
 
 console.log('Start')
